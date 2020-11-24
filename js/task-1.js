9610464d657234447b6f40266fe7e0e98cc28431
Maksim <lisovoi123@gmail.com>
2020-11-24 23:36:26 +0200

    Раскоментирует скрипты и апргрейдит задание №1

--- js/task-1.js
+++ js/task-1.js
@@ -1,6 +1,8 @@
-const items = document.querySelectorAll('.item')
+const items = document.querySelectorAll(".item");
+console.log(`В списке ${items.length} категории.`);
 
 items.forEach((el) => {
-    console.log(`Категория: ${el.children[0].textContent}, Количество элементов: ${el.children[1].childElementCount}`);
-})
-
+  console.log(
+    `Категория: ${el.children[0].textContent}, Количество элементов: ${el.children[1].childElementCount}`
+  );
+});
