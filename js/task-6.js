fc58b5346855e1db0139c259aba50524a2020295
Maksim <lisovoi123@gmail.com>
2020-11-24 23:44:26 +0200

    Фиксит таск №6

--- js/task-6.js
+++ js/task-6.js
@@ -2,7 +2,7 @@ const input = document.getElementById("validation-input");
 const inputLength = parseInt(input.attributes[2].value);
 
 input.addEventListener("blur", (event) => {
-  if (parseInt(event.target.value.length) !== inputLength) {
+  if (event.target.value.length !== inputLength) {
     input.classList.add("invalid");
     input.classList.remove("valid");
   } else {
